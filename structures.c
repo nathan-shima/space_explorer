f23ae2156785326516ea89ba0d9ccc07f7b5bfcb
nathan_shima_23031067 <nathanshima09@outlook.com>
2025-05-01 11:57:00 +0100

    started development

---
+++ structures.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct asteroid;
+int ast_x_location;
+int ast_y_location;
+
+struct ship;
+int ship_x_location;
+int ship_y_location;
+
+struct junk;
+int junk_x_location;
+int junk_y_location;
+
+struct game_board;
+int board_arr [18][18];
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
