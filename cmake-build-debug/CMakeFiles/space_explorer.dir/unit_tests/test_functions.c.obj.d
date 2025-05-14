372044386828e8582487e852777973aaa57c810c
Nathan shima <nathanshima09@outlook.com>
2025-05-14 18:11:05 +0100

    ...

---
+++ cmake-build-debug/CMakeFiles/space_explorer.dir/unit_tests/test_functions.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/space_explorer.dir/unit_tests/test_functions.c.obj: \
+ C:\Users\Natha\OneDrive\ -\ UWE\ Bristol\year\ 1\sem\ 2\programming\space\ explorer\unit_tests\test_functions.c \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/assert.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.3.3/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:\Users\Natha\OneDrive\ -\ UWE\ Bristol\year\ 1\sem\ 2\programming\space\ explorer\unit_tests\function_list.h \
+ C:\Users\Natha\OneDrive\ -\ UWE\ Bristol\year\ 1\sem\ 2\programming\space\ explorer\unit_tests\sprites.h
---
+++ cmake-build-debug/CMakeFiles/unit_tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_tests.dir/function_definition.c.obj"
+  "CMakeFiles/unit_tests.dir/function_definition.c.obj.d"
+  "CMakeFiles/unit_tests.dir/unit_tests/test_functions.c.obj"
+  "CMakeFiles/unit_tests.dir/unit_tests/test_functions.c.obj.d"
+  "libunit_tests.dll.a"
+  "unit_tests.exe"
+  "unit_tests.exe.manifest"
+  "unit_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unit_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
