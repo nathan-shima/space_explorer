bf81d812147958fb2370d9afaf9fb723312f4377
Nathan shima <nathanshima09@outlook.com>
2025-05-15 06:53:53 +0100

    ...

---
+++ cmake-build-debug/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Natha/OneDrive - UWE Bristol/year 1/sem 2/programming/space explorer/function_definition.c" "CMakeFiles/tests.dir/function_definition.c.obj" "gcc" "CMakeFiles/tests.dir/function_definition.c.obj.d"
+  "C:/Users/Natha/OneDrive - UWE Bristol/year 1/sem 2/programming/space explorer/test_functions.c" "CMakeFiles/tests.dir/test_functions.c.obj" "gcc" "CMakeFiles/tests.dir/test_functions.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/function_definition.c.obj"
+  "CMakeFiles/tests.dir/function_definition.c.obj.d"
+  "CMakeFiles/tests.dir/test_functions.c.obj"
+  "CMakeFiles/tests.dir/test_functions.c.obj.d"
+  "libtests.dll.a"
+  "tests.exe"
+  "tests.exe.manifest"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
